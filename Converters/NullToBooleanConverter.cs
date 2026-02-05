30ae2833142ee47ce422a37fe0c849cbd2fd63fa
Mhd Sukar <Epyion96.zx@outlook.com>
2026-02-05 19:11:44 +0300

    14215

--- Converters/NullToBooleanConverter.cs
+++ Converters/NullToBooleanConverter.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Windows.Data;
 
 namespace ESCenter.Converters
 {
     public class NullToBooleanConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, System.Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null; // true if null, false otherwise
+            return value == null;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, System.Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return System.Windows.Data.Binding.DoNothing;
         }
     }
-
 }
