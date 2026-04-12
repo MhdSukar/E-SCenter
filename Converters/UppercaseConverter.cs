659db1cd0140cd0edf034cc441da0f0c71550d33
Mhd Sukar <Epyion96.zx@outlook.com>
2026-04-12 06:08:08 +0300

    Style Redesign 12/04/2026

---
+++ Converters/UppercaseConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+namespace ESCenter.Converters;
+
+public class UppercaseConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value?.ToString().ToUpper();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value?.ToString().ToLower();
+    }
+}
