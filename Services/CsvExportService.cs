64a2913a3f2555e1ac6fcf2152269611080df13c
Mhd Sukar <Epyion96.zx@outlook.com>
2026-04-19 06:25:05 +0300

    Fix compile errors in CSV export and logging namespaces

--- Services/CsvExportService.cs
+++ Services/CsvExportService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32;
+using ESCenter.Core;
 
 namespace ESCenter.Services
 {
@@ -27,7 +28,7 @@ namespace ESCenter.Services
                     return;
                 }
 
-                await using var stream = new FileStream(dialog.FileName, FileMode.Create, FileAccess.Write, FileShare.None);
+                await using var stream = new FileStream(dialog.FileName, FileMode.Create, FileAccess.Write);
                 await using var writer = new StreamWriter(stream, new UTF8Encoding(true));
 
                 await writer.WriteLineAsync(string.Join(",", headers.Select(Escape)));
