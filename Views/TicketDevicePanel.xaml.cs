ed0aa19a890a90b2efdac502d220a2711e3b60f7
Mhd Sukar <Epyion96.zx@outlook.com>
2026-04-24 01:19:36 +0300

    Adjust repair ticket header actions and tab panel field placement

--- Views/TicketDevicePanel.xaml.cs
+++ Views/TicketDevicePanel.xaml.cs
@@ -1,6 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using ESCenter.ViewModels;
 
 namespace ESCenter.Views
 {
@@ -22,6 +23,30 @@ namespace ESCenter.Views
 
                 e.Handled = true;
             }
+        }
+
+        private void SaveOrAddOnEnter(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+            {
+                return;
+            }
+
+            if (DataContext is not RepairTicketsViewModel vm)
+            {
+                return;
+            }
+
+            if (vm.SelectedTicket != null && vm.SaveCommand.CanExecute(null))
+            {
+                vm.SaveCommand.Execute(null);
+            }
+            else if (vm.AddCommand.CanExecute(null))
+            {
+                vm.AddCommand.Execute(null);
+            }
+
+            e.Handled = true;
         }
     }
 }
